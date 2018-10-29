52e51dc8544ba5ecc1c514abd8c06909ebde3fa5
Jennifer Gonçalves <jenny_cg23@hotmail.com>
2018-10-29 11:05:10 -0300

    Criacao da view model da visao geral para gerenciar as view data

---
+++ CtrlMoney/ViewModel/VisaoGeralViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CtrlMoney.ViewModel
+{
+    public class VisaoGeralViewModel
+    {
+        public class DinnerFormViewModel
+        {
+            // site para visuzalizar
+            // https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions-1/nerddinner/use-viewdata-and-implement-viewmodel-classes
+
+            // Properties
+            public Dominio.Receita receita { get; private set; }
+            public Dominio.Despesa despesa { get; private set; }
+
+         
+
+
+
+    }
+}
