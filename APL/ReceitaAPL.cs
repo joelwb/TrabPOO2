f29f37eed0c01a1010c8c00d920c50b4a0254735
Jennifer Gonçalves <jenny_cg23@hotmail.com>
2018-11-05 10:49:39 -0200

    APL de despesa e receita

---
+++ APL/ReceitaAPL.cs
@@ -0,0 +1,57 @@
+﻿using Dominio;
+using EntityAcessoDados;
+using EntityAcessoDados.Repositorio;
+using Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace APL
+{
+    class ReceitaAPL : IAPLGenerica
+    {
+        private CtrlMoneyDbContext db;
+        private IRepositorioGenerico<Receita, string> repositorioReceita;
+
+
+        public ReceitaAPL()
+        {
+            db = new CtrlMoneyDbContext();
+            repositorioReceita = new ReceitaReposEntity(db);
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public void Inserir(Receita Receita)
+        {
+
+            repositorioReceita.Inserir(Receita);
+        }
+
+        //public Receita SelecionarById(string id)
+        //{
+        //    return repositorioReceita.SelecionarPorId(id);
+        //}
+
+        public void Alterar(Receita Receita)
+        {
+
+            repositorioReceita.Alterar(Receita);
+        }
+
+        public void Deletar(Receita Receita)
+        {
+
+            repositorioReceita.Excluir(Receita);
+        }
+    }
+}
+
+
+   
---
+++ EntityAcessoDados/Repositorio/DespesaReposEntity.cs
@@ -0,0 +1,43 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EntityAcessoDados.Repositorio
+{
+    public class DespesaReposEntity : RepositorioGenericoEntity<Despesa, string>
+    { 
+     public DespesaReposEntity(DbContext contexto) : base(contexto)
+    {
+    }
+
+    public override void Inserir(Despesa entidade)
+    {
+        _contexto.Set<Despesa>().Add(entidade);
+        _contexto.SaveChanges();
+    }
+
+    //public override Despesa SelecionarPorId(string id)
+    //{
+    //    return _contexto.Set<Despesa>().Include(p => p.Pessoa).SingleOrDefault(p => p.Id == id);
+    //}
+
+    public override void Alterar(Despesa entidade)
+    {
+        _contexto.Set<Despesa>().Attach(entidade);
+        _contexto.Entry(entidade).State = EntityState.Modified;
+        _contexto.Entry(entidade.Pessoa).State = EntityState.Modified;
+        _contexto.SaveChanges();
+    }
+
+    public override void Excluir(Despesa entidade)
+    {
+        _contexto.Set<Despesa>().Attach(entidade);
+        _contexto.Entry(entidade.Pessoa).State = EntityState.Deleted;
+        _contexto.Entry(entidade).State = EntityState.Deleted;
+        _contexto.SaveChanges();
+    }
+}
+}
