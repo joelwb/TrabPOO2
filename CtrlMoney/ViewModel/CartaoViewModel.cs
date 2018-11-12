df4e0682760cb561585fafe2d5665337a7fe2012
brendonmauro <brendonmauro@hotmail.com>
2018-11-11 23:55:41 -0200

    Melhora básica do visual do CRUD cartão

--- CtrlMoney/ViewModel/CartaoViewModel.cs
+++ CtrlMoney/ViewModel/CartaoViewModel.cs
@@ -11,24 +11,29 @@ namespace CtrlMoney.ViewModel
 
         [Required(ErrorMessage = "Nome é um campo obrigatório")]
         [JsonProperty("nome")]
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "Limite é um campo obrigatório")]
         [JsonProperty("limite")]
+        [Display(Name = "Limite")]
         public decimal Limite { get; set; }
 
         [Required(ErrorMessage = "Dia de fechamento é um campo obrigatório")]
         [JsonProperty("diaFechamento")]
         [Range(1, 28, ErrorMessage = "O dia tem que ser entre 1 e 28.")]
+        [Display(Name = "Dia de Fechamento")]
         public int DiaFechamento { get; set; }
 
         [Required(ErrorMessage = "Vencimento é um campo obrigatório")]
         [JsonProperty("diaVencimento")]
         [Range(1, 28, ErrorMessage = "O dia tem que ser entre 1 e 28.")]
+        [Display(Name = "Dia de Vencimento")]
         public int DiaVencimento { get; set; }
 
         [Required(ErrorMessage = "Numero é um campo obrigatório")]
         [JsonProperty("numero")]
+        [Display(Name = "Número do Cartão")]
         public long Numero { get; set; }
     }
 }
