34c3f0756ed9ce603b662ce5fc10a11df9a39f1a
brendonmauro <brendonmauro@hotmail.com>
2018-12-02 17:42:10 -0200

    versao final domingo

--- EntityAcessoDados/Migrations/201812021858286_migration_final.Designer.cs
+++ EntityAcessoDados/Migrations/201812021858286_migration_final.Designer.cs
@@ -7,13 +7,13 @@ namespace EntityAcessoDados.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class Migration2 : IMigrationMetadata
+    public sealed partial class migration_final : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Migration2));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(migration_final));
         
         string IMigrationMetadata.Id
         {
-            get { return "201811111834523_Migration2"; }
+            get { return "201812021858286_migration_final"; }
         }
         
         string IMigrationMetadata.Source
--- EntityAcessoDados/Migrations/201812021858286_migration_final.cs
+++ EntityAcessoDados/Migrations/201812021858286_migration_final.cs
@@ -3,7 +3,7 @@ namespace EntityAcessoDados.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class Migration1 : DbMigration
+    public partial class migration_final : DbMigration
     {
         public override void Up()
         {
@@ -21,21 +21,6 @@ namespace EntityAcessoDados.Migrations
                 .PrimaryKey(t => t.id);
             
             CreateTable(
-                "public.pessoa_usuario",
-                c => new
-                    {
-                        id = c.String(nullable: false, maxLength: 128),
-                        nome = c.String(nullable: false, maxLength: 150),
-                        cpf = c.String(nullable: false, maxLength: 11, fixedLength: true),
-                        data_nasc = c.DateTime(nullable: false),
-                        login = c.String(nullable: false, maxLength: 150),
-                        senha = c.String(nullable: false, maxLength: 128),
-                        data_cadastro = c.DateTime(nullable: false),
-                    })
-                .PrimaryKey(t => t.id)
-                .Index(t => t.login, unique: true);
-            
-            CreateTable(
                 "public.despesa",
                 c => new
                     {
@@ -51,6 +36,21 @@ namespace EntityAcessoDados.Migrations
                 .PrimaryKey(t => t.id)
                 .ForeignKey("public.pessoa_usuario", t => t.PessoaId, cascadeDelete: true)
                 .Index(t => t.PessoaId);
+            
+            CreateTable(
+                "public.pessoa_usuario",
+                c => new
+                    {
+                        id = c.String(nullable: false, maxLength: 128),
+                        nome = c.String(nullable: false, maxLength: 150),
+                        cpf = c.String(nullable: false, maxLength: 11, fixedLength: true),
+                        data_nasc = c.DateTime(nullable: false),
+                        login = c.String(nullable: false, maxLength: 150),
+                        senha = c.String(nullable: false, maxLength: 128),
+                        data_cadastro = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.id)
+                .Index(t => t.login, unique: true);
             
             CreateTable(
                 "public.receita",
@@ -86,14 +86,14 @@ namespace EntityAcessoDados.Migrations
                 c => new
                     {
                         id = c.Int(nullable: false),
-                        Cartao_Id = c.Int(nullable: false),
                         num_parcelas = c.Short(nullable: false),
+                        CartaoId = c.Int(nullable: false),
                     })
                 .PrimaryKey(t => t.id)
                 .ForeignKey("public.despesa", t => t.id)
-                .ForeignKey("public.cartao", t => t.Cartao_Id, cascadeDelete: true)
+                .ForeignKey("public.cartao", t => t.CartaoId, cascadeDelete: true)
                 .Index(t => t.id)
-                .Index(t => t.Cartao_Id);
+                .Index(t => t.CartaoId);
             
             CreateTable(
                 "public.sem_parcelamento",
@@ -111,26 +111,26 @@ namespace EntityAcessoDados.Migrations
         public override void Down()
         {
             DropForeignKey("public.sem_parcelamento", "id", "public.despesa");
-            DropForeignKey("public.parcelamento", "Cartao_Id", "public.cartao");
+            DropForeignKey("public.parcelamento", "CartaoId", "public.cartao");
             DropForeignKey("public.parcelamento", "id", "public.despesa");
             DropForeignKey("public.receita", "PessoaId", "public.pessoa_usuario");
             DropForeignKey("public.despesa", "PessoaId", "public.pessoa_usuario");
             DropForeignKey("public.pessoa_cartao", "fk_cartao", "public.cartao");
             DropForeignKey("public.pessoa_cartao", "fk_pessoa_usuario", "public.pessoa_usuario");
             DropIndex("public.sem_parcelamento", new[] { "id" });
-            DropIndex("public.parcelamento", new[] { "Cartao_Id" });
+            DropIndex("public.parcelamento", new[] { "CartaoId" });
             DropIndex("public.parcelamento", new[] { "id" });
             DropIndex("public.pessoa_cartao", new[] { "fk_cartao" });
             DropIndex("public.pessoa_cartao", new[] { "fk_pessoa_usuario" });
             DropIndex("public.receita", new[] { "PessoaId" });
-            DropIndex("public.despesa", new[] { "PessoaId" });
             DropIndex("public.pessoa_usuario", new[] { "login" });
+            DropIndex("public.despesa", new[] { "PessoaId" });
             DropTable("public.sem_parcelamento");
             DropTable("public.parcelamento");
             DropTable("public.pessoa_cartao");
             DropTable("public.receita");
-            DropTable("public.despesa");
             DropTable("public.pessoa_usuario");
+            DropTable("public.despesa");
             DropTable("public.cartao");
         }
     }
